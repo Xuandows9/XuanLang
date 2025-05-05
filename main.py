16a59f93bfe2aec68556caf6332d850a31bc503d
Xuandows9 <2147138416@qq.com>
2025-05-05 15:34:51 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import sys,os,string
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class Code:
+    def __init__(self,codestring):
+        self.codestring=codestring
+        try:
+            self.args=codestring.split("(")[1][0:-1]
+            self.func=codestring.split("(")[0]
+            self.run()
+        except:
+            print(f" {bcolors.FAIL}{bcolors.BOLD}语法错误!{bcolors.ENDC}\n   {bcolors.BOLD}文件:{bcolors.ENDC}{bcolors.OKBLUE}\"{filename}\"{bcolors.ENDC}\n     {bcolors.BOLD}代码：{bcolors.WARNING}\"{self.codestring}\"{bcolors.ENDC}\n            {bcolors.FAIL}{'^'*len(self.codestring)}{bcolors.ENDC}")
+            exit()
+    def  run(self):
+        print(f"{'正在运行  代码：'+self.codestring+'，方法：'+self.func+'，参数：'+self.args}")
+
+        
+def main():
+    try:
+        f=open(filename,"r")
+    except:
+        print(f" {bcolors.FAIL}{bcolors.BOLD}IO错误!{bcolors.ENDC}\n  找不到文件：{bcolors.OKBLUE}\"{filename}\"{bcolors.ENDC}")
+        exit()
+    codes=[]
+    codes=f.read()
+    codes=codes.replace("\n","")
+    codes=codes.split(";")
+    codes=codes[0:-1] if codes[-1]=="" else codes
+    for code in codes:
+        Code(code)
+
+
+
+
+if __name__ =="__main__":
+    args=sys.argv
+    for i in range(len(args)):
+        if args[i]=="-f":
+            filename=args[i+1]
+
+    main()
